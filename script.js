d789f5f80734f5b972d8310bd9cd7b7f03edfa52
vedansh7002 <beastboyshubh1999@gmail.com>
2026-07-31 15:29:12 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,60 @@
+function openLetter(){
+
+document.getElementById("letter")
+.style.display="flex";
+
+}
+
+
+function closeLetter(){
+
+document.getElementById("letter")
+.style.display="none";
+
+}
+
+
+
+function playMusic(){
+
+let song=document.getElementById("song");
+
+song.play();
+
+}
+
+
+// floating hearts
+
+setInterval(()=>{
+
+let heart=document.createElement("div");
+
+heart.innerHTML="❤️";
+
+heart.style.position="fixed";
+
+heart.style.left=Math.random()*100+"%";
+
+heart.style.bottom="-20px";
+
+heart.style.fontSize=
+Math.random()*30+20+"px";
+
+heart.style.animation=
+"rise 5s linear";
+
+
+document.body.appendChild(heart);
+
+
+setTimeout(()=>{
+
+heart.remove();
+
+},5000);
+
+
+},300);
+
+
